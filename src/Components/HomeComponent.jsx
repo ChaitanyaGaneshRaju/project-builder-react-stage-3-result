1ffd1c90a194f4f84047b8f15c1a2ffe6f7ff6b0
Chaitanya Ganesh <chaitanyaraju2000@gmail.com>
2021-04-06 14:40:37 +0530

    3 tasks have been completed

---
+++ src/Components/HomeComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import '../Css/HomeComponent.css'
+
+class HomeComponent extends Component {
+  render() {
+    return (
+      <div>
+        <div className="home-component">
+          <h1>Quiz App</h1>
+          <button>play</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomeComponent;
